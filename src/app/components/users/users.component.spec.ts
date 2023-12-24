8e9754ed681f2ee895d5aa1bce30e8f9d702923a
e.ardalani <elnaz.ardalani66262@gmail.com>
2023-12-24 14:53:21 +0330

    Access Component Template using TestBed methods nativeElement & debugElement

--- src/app/components/users/users.component.spec.ts
+++ src/app/components/users/users.component.spec.ts
@@ -1,13 +1,17 @@
 import {UsersComponent} from "./users.component";
 import {of} from "rxjs";
 import {User} from "../../models/user.model";
-import {TestBed} from "@angular/core/testing";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
 import {UserService} from "../../services/user.service";
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 describe('User Component', () => {
   let users: User[];
-  let component: UsersComponent;
   let mockUserService: any;
+  let fixture: ComponentFixture<UsersComponent>;
+  let comp: UsersComponent;
+
 
   beforeEach(() => {
     users = [{
@@ -38,26 +42,43 @@ describe('User Component', () => {
         provide: UserService, useValue: mockUserService
       }]
     })
-    component = TestBed.inject(UsersComponent)
+    fixture = TestBed.createComponent(UsersComponent)
+    comp = fixture.componentInstance;
   });
+
+
+  it('should find <p> with fixture.debugElement.nativeElement', () => {
+    const itemDe: DebugElement = fixture.debugElement;
+    const itemEl: HTMLElement = itemDe.nativeElement;
+    const p = itemEl.querySelector('p');
+    expect(p?.textContent).toEqual('User List');
+  })
+
+  it('should find <p> with fixture.debugElement.query(By.css)', () => {
+    const itemDe: DebugElement = fixture.debugElement;
+    const itemEl = itemDe.query(By.css('p'));
+    const p:HTMLElement = itemEl.nativeElement;
+    expect(p?.textContent).toEqual('User List');
+  })
+
   describe('delete', () => {
     beforeEach(() => {
       mockUserService.deleteUser.and.returnValue(of(true));
-      component.users = users;
+      comp.users = users;
     })
     it('should delete select user from the users', () => {
-      component.deleteUser(users[1]);
-      expect(component.users.length).toBe(2);
+      comp.deleteUser(users[1]);
+      expect(comp.users.length).toBe(2);
     })
 
     it('should the call the delete method in user service only once', () => {
-      component.deleteUser(users[1]);
+      comp.deleteUser(users[1]);
       expect(mockUserService.deleteUser).toHaveBeenCalledTimes(1);
     })
 
     it('should delete the actual selected user in users', () => {
-      component.deleteUser(users[1]);
-      for (let user of component.users) {
+      comp.deleteUser(users[1]);
+      for (let user of comp.users) {
         expect(user).not.toEqual(users[1])
       }
     })
