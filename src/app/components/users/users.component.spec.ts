7431d4cea69f846eac04bf7bcfaf9278e35a8fa2
e.ardalani <elnaz.ardalani66262@gmail.com>
2023-12-25 11:31:09 +0330

    testing whether the child component created only one for each user

--- src/app/components/users/users.component.spec.ts
+++ src/app/components/users/users.component.spec.ts
@@ -6,7 +6,7 @@ import {UserService} from "../../services/user.service";
 import {DebugElement} from "@angular/core";
 import {By} from "@angular/platform-browser";
 
-describe('User Component', () => {
+describe('Users Component', () => {
   let users: User[];
   let mockUserService: any;
   let fixture: ComponentFixture<UsersComponent>;
@@ -46,6 +46,14 @@ describe('User Component', () => {
     comp = fixture.componentInstance;
   });
 
+  it('should create on user child element for each user', () => {
+    mockUserService.getUsers.and.returnValue(of(users))
+    fixture.detectChanges();
+    const debugElement=fixture.debugElement;
+    const usersElement=debugElement.queryAll(By.css('.users'))
+    expect(usersElement.length).toBe(users.length)
+  })
+
 
   it('should find <p> with fixture.debugElement.nativeElement', () => {
     const itemDe: DebugElement = fixture.debugElement;
@@ -57,10 +65,15 @@ describe('User Component', () => {
   it('should find <p> with fixture.debugElement.query(By.css)', () => {
     const itemDe: DebugElement = fixture.debugElement;
     const itemEl = itemDe.query(By.css('p'));
-    const p:HTMLElement = itemEl.nativeElement;
+    const p: HTMLElement = itemEl.nativeElement;
     expect(p?.textContent).toEqual('User List');
   })
 
+  it('should set users from the service directly', () => {
+    mockUserService.getUsers.and.returnValue(of(users))
+    fixture.detectChanges();
+    expect(comp.users.length).toBe(3)
+  })
   describe('delete', () => {
     beforeEach(() => {
       mockUserService.deleteUser.and.returnValue(of(true));
